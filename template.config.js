b3e4d96546d1feb412618f1b998e492423ece8d1
Jennifer Robison <jenn@echobind.com>
2019-11-04 11:50:46 -0800

    try original script with node shebang`

--- template.config.js
+++ template.config.js
@@ -3,5 +3,5 @@ module.exports = {
   titlePlaceholder: "Hello World",
   templateDir: "./template",
   // Path to script, which will be executed after initialization process, but before installing all the dependencies specified in the template. This script runs as a shell script but you can change that (e.g. to Node) by using a shebang (see example custom template).
-  postInitScript: './postInitScript.js',
+  postInitScript: './setup/index.js',
 };
